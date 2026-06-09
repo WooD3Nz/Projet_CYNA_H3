d720eafb1d0e62ef9bdb1fd67ad1efa31c6a0e17
Enzo V <viriato.enzo@gmail.com>
2026-06-09 23:14:05 +0200

    Add outputs for Azure resources in outputs.tf

---
+++ infra/terraform/azure/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "frontend_subnet_id" {
+  value = azurerm_subnet.frontend.id
+}
+
+output "backend_subnet_id" {
+  value = azurerm_subnet.backend.id
+}
+
+output "monitoring_subnet_id" {
+  value = azurerm_subnet.monitoring.id
+}
