4d893a5fc9ecbf7c04bdecec68f1543688dd4525
Enzo V <viriato.enzo@gmail.com>
2026-06-09 23:13:44 +0200

    Add Azure infrastructure resources in main.tf
    Define Azure resources including a resource group, virtual network, and multiple subnets.

---
+++ infra/terraform/azure/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.project_name}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "subnet-frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.10.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "subnet-backend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.20.0/24"]
+}
+
+resource "azurerm_subnet" "monitoring" {
+  name                 = "subnet-monitoring"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.30.0/24"]
+}
